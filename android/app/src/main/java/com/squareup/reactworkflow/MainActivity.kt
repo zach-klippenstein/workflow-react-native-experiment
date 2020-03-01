da40e453ff9c1d177c97251657ba73b3a66abba6
Zach Klippenstein <zachklipp@squareup.com>
2020-03-01 12:40:20 -0800

    Got app running.

--- android/app/src/main/java/com/squareup/reactworkflow/MainActivity.kt
+++ android/app/src/main/java/com/squareup/reactworkflow/MainActivity.kt
@@ -2,13 +2,48 @@ package com.squareup.reactworkflow
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.facebook.react.ReactInstanceManager
+import com.facebook.react.ReactRootView
+import com.facebook.react.common.LifecycleState
+import com.facebook.react.shell.MainReactPackage
+import com.facebook.soloader.SoLoader
 
 class MainActivity : AppCompatActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(
-        R.layout
-            .activity_main
-    )
-  }
+
+    private val rootView by lazy { ReactRootView(this) }
+    private val instanceManager by lazy {
+        ReactInstanceManager.builder()
+            .setApplication(application)
+            .setCurrentActivity(this)
+            .setBundleAssetName("index.android.bundle")
+            .setJSMainModulePath("index")
+            .addPackage(MainReactPackage())
+            .setUseDeveloperSupport(BuildConfig.DEBUG)
+            .setInitialLifecycleState(LifecycleState.RESUMED)
+            .build()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        SoLoader.init(this, false)
+        println(instanceManager)
+        rootView.startReactApplication(instanceManager, "MyReactNativeApp", null)
+        setContentView(rootView)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        instanceManager.onHostResume(this, ::onBackPressed)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        instanceManager.onHostPause(this)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        instanceManager.onHostDestroy(this)
+        rootView.unmountReactApplication()
+    }
 }
